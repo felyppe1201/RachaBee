641230bb039c7e110e37dd0c2fe50528e60d22ad
senna <joaopedrorochasenna@gmail.com>
2026-05-17 23:27:21 -0300

    Feat: Tab navegation & +Theme colors

---
+++ src/pages/perfil/perfil.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Perfil() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="font-bold text-lg">Perfil</Text>
+    </View>
+  );
+}
